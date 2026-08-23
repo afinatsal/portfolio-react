96a3251e3ae0329a3596d3da15d414749671580c
afinatsal <afinatsal@student.ub.ac.id>
2026-08-23 10:00:13 +0700

    Drive scroll line with JS for cross-browser animation

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ import {
   initMusicPlayer,
   initChatbot,
   initProjectLinks,
+  initScrollLine,
 } from './behaviors/index.js'
 
 const MARKUP = [
@@ -69,6 +70,7 @@ export default function App() {
     initMusicPlayer()
     initChatbot()
     initProjectLinks()
+    initScrollLine()
 
     return () => {
       document.body.classList.remove('modal-open')
